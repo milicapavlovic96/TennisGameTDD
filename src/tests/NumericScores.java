1c33527d4a32eb14402e206fdeb7f1d97dfbd72a
somename <someemail>
2017-05-18 20:15:02 +0200

    besouro automatic message

--- src/tests/NumericScores.java
+++ src/tests/NumericScores.java
@@ -81,5 +81,15 @@ public class NumericScores {
 		String score = game.getScore();
 		assertEquals("Initial score incorrect", "15 - 15", score);	
 	}
+	
+	@Test
+	public void test_player1_scored_twice_player2_scored_once() {
+		TennisGame game = new TennisGame();
+		game.player1Scored();
+		game.player1Scored();
+		game.player2Scored();
+		String score = game.getScore();
+		assertEquals("Initial score incorrect", "30 - 15", score);	
+	}
 
 }
